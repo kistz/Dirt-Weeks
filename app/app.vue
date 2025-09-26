db5956cfa0a62bf9af566f39d9d6921901c47e86
kistz (Kilian Strunz) <kilianstrunz@gmx.de>
2025-09-26 20:25:41 +0200

    add bare bones keycloak

--- app/app.vue
+++ app/app.vue
@@ -20,9 +20,13 @@ const events:Ref<Array<Event>>= ref([]);
 onBeforeMount( () => {
   DbConnection.builder().onConnect((connection: DbConnection, identity: Identity, token: string) => { 
 
-    connection.subscriptionBuilder().onApplied((ctx) => {
-      console.log("SDK client cache initialized.");
-    }).onError(() => console.log("that is yek")).subscribe("SELECT * FROM server_events")
+    connection.subscriptionBuilder()
+      .onApplied((ctx) => {
+        console.log("Spacetime client cache initialized.");
+      })
+      .onError(() => 
+        console.log("that is yek"))
+      .subscribe("SELECT * FROM server_events")
 
     connection.db.serverEvents.onInsert((ctx, row) => {
       events.value.push(row.typed)
